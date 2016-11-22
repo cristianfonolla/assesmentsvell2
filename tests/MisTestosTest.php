8c606999ca509a9c54c1594886ce7be87939d3aa
cristian <cristian.fonolla@gmail.com>
2016-11-22 16:30:16 +0100

    test en verd

---
+++ tests/MisTestosTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: cristian
+ * Date: 22/11/16
+ * Time: 16:16
+ */
+class MisTestosTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Mi prueba tonta
+     * @test
+     */
+    public function it_shows_submodules()
+    {
+
+        //Preparacio
+        //Execucio
+        //Assertions
+
+
+    }
+
+}
